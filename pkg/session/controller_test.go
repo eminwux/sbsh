4312dc8e5cb6e8950f6cc81e1c50a6d818711d41
Emiliano Spinella <emilianofs@gmail.com>
2025-09-18 22:58:07 +0200

    testing: sbsh-session Test_ContextDone

--- pkg/session/controller_test.go
+++ pkg/session/controller_test.go
@@ -205,3 +205,54 @@ func Test_StartSessionFailed(t *testing.T) {
 	}
 
 }
+
+func Test_ContextDone(t *testing.T) {
+	exitCh := make(chan error)
+	ctx, cancel := context.WithCancel(context.Background())
+	sessionCtrl := NewSessionController(ctx, exitCh)
+
+	// Define a new Session
+	spec := api.SessionSpec{
+		ID:          api.SessionID("abcdef"),
+		Kind:        api.SessLocal,
+		Label:       "default",
+		Command:     "/bin/bash",
+		CommandArgs: nil,
+		Env:         os.Environ(),
+		LogDir:      "/tmp/sbsh-logs/s0",
+	}
+
+	newSessionRunner = func(spec *api.SessionSpec) sessionrunner.SessionRunner {
+		return &sessionrunner.SessionRunnerTest{
+			IDFunc: func() api.SessionID {
+				return spec.ID
+			},
+			OpenSocketCtrlFunc: func() (net.Listener, error) {
+				return newStubListener(), nil
+			},
+			StartServerFunc: func(ctx context.Context, ln net.Listener, sc *sessionrpc.SessionControllerRPC, readyCh chan error, doneCh chan error) {
+				readyCh <- nil
+			},
+			StartSessionFunc: func(ctx context.Context, evCh chan<- api.SessionEvent) error {
+				return nil
+			},
+		}
+	}
+
+	var buf bytes.Buffer
+	old := log.Writer()
+	log.SetOutput(&buf)
+	t.Cleanup(func() { log.SetOutput(old) })
+
+	go sessionCtrl.Run(&spec)
+
+	time.Sleep(10 * time.Millisecond)
+	cancel()
+	time.Sleep(10 * time.Millisecond)
+
+	message := "parent context channel has been closed"
+	if !bytes.Contains(buf.Bytes(), []byte(message)) {
+		t.Fatalf("expected '"+message+"' in logs; got: %s", buf.String())
+	}
+
+}
