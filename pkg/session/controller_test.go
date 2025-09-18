dc76457a6e3b55f8c7517ec3d48017fa8da55a89
Emiliano Spinella <emilianofs@gmail.com>
2025-09-18 22:43:25 +0200

    testing: sbsh-session Test_EmptySpecCmd

--- pkg/session/controller_test.go
+++ pkg/session/controller_test.go
@@ -29,7 +29,48 @@ func (f *fakeListener) Addr() net.Addr {
 // use in test
 func newStubListener() net.Listener { return &fakeListener{} }
 
-// ////////////////////////////////////////////////////////
+func Test_EmptySpecCmd(t *testing.T) {
+	exitCh := make(chan error)
+	sessionCtrl := NewSessionController(context.Background(), exitCh)
+
+	// Define a new Session
+	spec := api.SessionSpec{
+		ID:          api.SessionID("abcdef"),
+		Kind:        api.SessLocal,
+		Label:       "default",
+		Command:     "",
+		CommandArgs: nil,
+		Env:         os.Environ(),
+		LogDir:      "/tmp/sbsh-logs/s0",
+	}
+
+	newSessionRunner = func(spec *api.SessionSpec) sessionrunner.SessionRunner {
+		return &sessionrunner.SessionRunnerTest{
+			IDFunc: func() api.SessionID {
+				return api.SessionID("iajs099")
+			},
+			OpenSocketCtrlFunc: func() (net.Listener, error) {
+				return newStubListener(), fmt.Errorf("error opening listener")
+			},
+		}
+	}
+
+	var buf bytes.Buffer
+	old := log.Writer()
+	log.SetOutput(&buf)
+	t.Cleanup(func() { log.SetOutput(old) })
+
+	go sessionCtrl.Run(&spec)
+
+	time.Sleep(100 * time.Millisecond)
+
+	message := "empty command in SessionSpec"
+	if !bytes.Contains(buf.Bytes(), []byte(message)) {
+		t.Fatalf("expected '"+message+"' in logs; got: %s", buf.String())
+	}
+
+}
+
 func Test_OpenSocketCtrlError(t *testing.T) {
 	exitCh := make(chan error)
 	sessionCtrl := NewSessionController(context.Background(), exitCh)
