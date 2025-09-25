061f55aa3048a19b629fb2ed82e48bb0c821a19d
Emiliano Spinella <emilianofs@gmail.com>
2025-09-25 18:19:34 +0200

    feat: added logging sb

--- cmd/sb/main.go
+++ cmd/sb/main.go
@@ -12,7 +12,6 @@ import (
 	"os"
 	"path/filepath"
 	"sbsh/cmd/sb/sessions"
-	"sbsh/pkg/client"
 	"sbsh/pkg/common"
 
 	"github.com/spf13/cobra"
@@ -23,18 +22,27 @@ func main() {
 	Execute()
 }
 
-var logLevel string
+var (
+	logLevel string
+	runPath  string
+	cfgFile  string
+)
 
-// RootCmd represents the base command when called without any subcommands
-var RootCmd = &cobra.Command{
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
 	Use:   "sb",
 	Short: "A brief description of your application",
 	Long:  `A longer description ...`,
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		viper.BindEnv("SBSH_LOG_LEVEL")
-		logLevel = viper.GetString("SBSH_LOG_LEVEL")
+		err := LoadConfig()
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Config error:", err)
+			os.Exit(1)
+		}
+
 		h := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-			Level: common.ParseLevel(logLevel),
+			Level: common.ParseLevel(viper.GetString("global.logLevel")),
 		})
 		slog.SetDefault(slog.New(h))
 		return nil
@@ -54,33 +62,34 @@ var RootCmd = &cobra.Command{
 func run() {
 	_, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	// Create a new Controller
-	// var ctrl api.Controller
-
-	_, err := client.NewController("/home/inwx/.sbsh/socket")
-	if err != nil {
-		log.Printf("[client] Supervisor not running: %v ", err)
-		return
-	} else {
-		log.Printf("[client] Supervisor is running: %v ", err)
-
-	}
-
 }
 
 ///////////////////////////////////////////////
 
 func Execute() {
-	err := RootCmd.Execute()
+	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
-	RootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "info", "Log level (debug, info, warn, error)")
-	RootCmd.AddCommand(sessions.SessionsCmd)
+	rootCmd.AddCommand(sessions.SessionsCmd)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sbsh/config.yaml)")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "info", "Log level (debug, info, warn, error)")
+	rootCmd.PersistentFlags().StringVar(&runPath, "run-path", "$HOME/.sbsh/run", "Log level (debug, info, warn, error)")
+
+	// Bind flag to Viper
+	if err := viper.BindPFlag("global.config", rootCmd.PersistentFlags().Lookup("config")); err != nil {
+		log.Fatal(err)
+	}
+	if err := viper.BindPFlag("global.logLevel", rootCmd.PersistentFlags().Lookup("log-level")); err != nil {
+		log.Fatal(err)
+	}
+	if err := viper.BindPFlag("global.runPath", rootCmd.PersistentFlags().Lookup("run-path")); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // LoadConfig loads config.yaml from the given path or HOME/.sbsh
@@ -88,14 +97,18 @@ func LoadConfig() error {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 
-	// Then ~/.sbsh
 	home, err := os.UserHomeDir()
-	if err == nil {
-		viper.AddConfigPath(filepath.Join(home, ".sbsh"))
-	}
+	if err != nil {
+		log.Fatalf("err: %v", err)
 
-	// Bind ENV variable as a fallback
-	// viper.BindEnv("SBSH_LOG_LEVEL")
+	}
+	viper.AddConfigPath(filepath.Join(home, ".sbsh"))
+
+	viper.BindEnv("global.logLevel", "SBSH_LOG_LEVEL")
+	viper.BindEnv("global.runPath", "SBSH_RUN_PATH")
+
+	viper.SetDefault("global.logLevel", "info")
+	viper.SetDefault("global.runPath", filepath.Join(home, ".sbsh", "run"))
 
 	if err := viper.ReadInConfig(); err != nil {
 		// File not found is OK if ENV is set
@@ -105,5 +118,6 @@ func LoadConfig() error {
 			return err // Config file was found but another error was produced
 		}
 	}
+
 	return nil
 }
